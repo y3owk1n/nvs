e7f7ad4c66b52c9011649574c8cdac5a04f780b2
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2025-03-07 08:37:55 +0800

    feat: make list nicer with table (#35)

--- cmd/list-installed.go
+++ cmd/list-installed.go
@@ -2,6 +2,8 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+	"text/tabwriter"
 
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
@@ -18,19 +20,27 @@ var listInstalledCmd = &cobra.Command{
 			logrus.Fatalf("Error listing versions: %v", err)
 		}
 
-		// Try to determine the current version.
 		current, err := utils.GetCurrentVersion(versionsDir)
 		if err != nil {
 			logrus.Warn("No current version set or unable to determine the current version")
+			current = "none"
 		}
 
+		// Create a tab writer for a formatted table.
+		w := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', 0)
+		// Table header.
+		fmt.Fprintln(w, "VERSION\tSTATUS")
+		fmt.Fprintln(w, "-------\t------")
 		for _, v := range versions {
+			status := ""
 			if v == current {
-				fmt.Printf("%s (current)\n", v)
+				status = "Current"
 			} else {
-				fmt.Println(v)
+				status = "Installed"
 			}
+			fmt.Fprintf(w, "%s\t%s\n", v, status)
 		}
+		w.Flush()
 	},
 }
 
