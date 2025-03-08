11941578097de4e8e1ae6426795275d817a8570e
y3owk1n <wongyeowkin@gmail.com>
2025-03-08 18:55:28 +0800

    feat: improve currentCmd UI

--- cmd/current.go
+++ cmd/current.go
@@ -2,21 +2,49 @@ package cmd
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
+	"github.com/y3owk1n/nvs/pkg/releases"
 	"github.com/y3owk1n/nvs/pkg/utils"
 )
 
 var currentCmd = &cobra.Command{
 	Use:   "current",
-	Short: "Show current active version",
+	Short: "Show current active version with details",
 	Run: func(cmd *cobra.Command, args []string) {
 		current, err := utils.GetCurrentVersion(versionsDir)
 		if err != nil {
 			logrus.Fatalf("Error getting current version: %v", err)
 		}
-		fmt.Println(current)
+
+		switch current {
+		case "stable":
+			stable, err := releases.FindLatestStable(cacheFilePath)
+			if err != nil {
+				fmt.Printf("stable (%s)\n", current)
+			} else {
+				fmt.Printf("stable\n  Version: %s\n", stable.TagName)
+			}
+		case "nightly":
+			nightly, err := releases.FindLatestNightly(cacheFilePath)
+			if err != nil {
+				fmt.Printf("nightly (%s)\n", current)
+			} else {
+				shortCommit := nightly.CommitHash
+				if len(shortCommit) > 10 {
+					shortCommit = shortCommit[:10]
+				}
+				publishedStr := nightly.PublishedAt
+				if t, err := time.Parse(time.RFC3339, nightly.PublishedAt); err == nil {
+					publishedStr = t.Format("2006-01-02")
+				}
+				fmt.Printf("nightly\n  Published: %s\n  Commit: %s\n", publishedStr, shortCommit)
+			}
+		default:
+			fmt.Printf("%s\n", current)
+		}
 	},
 }
 
