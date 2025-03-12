538a362a74217fb74804a0f1399bb85511b637ee
y3owk1n <wongyeowkin@gmail.com>
2025-03-12 23:03:02 +0800

    fix: add more debug logs for verbose mode for `nvs env` and `nvs list`

--- cmd/env.go
+++ cmd/env.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/fatih/color"
 	"github.com/olekukonko/tablewriter"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
@@ -14,32 +15,40 @@ var envCmd = &cobra.Command{
 	Short: "Print NVS env configurations",
 	Long:  "Prints the env configuration used by NVS (NVS_CONFIG_DIR, NVS_CACHE_DIR, and NVS_BIN_DIR).",
 	Run: func(cmd *cobra.Command, args []string) {
+		logrus.Debug("Executing env command")
+
 		configDir := os.Getenv("NVS_CONFIG_DIR")
 		if configDir == "" {
 			if c, err := os.UserConfigDir(); err == nil {
 				configDir = filepath.Join(c, "nvs")
 			} else {
+				logrus.Warn("Failed to retrieve user config directory")
 				configDir = "Unavailable"
 			}
 		}
+		logrus.Debugf("Resolved configDir: %s", configDir)
 
 		cacheDir := os.Getenv("NVS_CACHE_DIR")
 		if cacheDir == "" {
 			if c, err := os.UserCacheDir(); err == nil {
 				cacheDir = filepath.Join(c, "nvs")
 			} else {
+				logrus.Warn("Failed to retrieve user cache directory")
 				cacheDir = "Unavailable"
 			}
 		}
+		logrus.Debugf("Resolved cacheDir: %s", cacheDir)
 
 		binDir := os.Getenv("NVS_BIN_DIR")
 		if binDir == "" {
 			if home, err := os.UserHomeDir(); err == nil {
 				binDir = filepath.Join(home, ".local", "bin")
 			} else {
+				logrus.Warn("Failed to retrieve user home directory")
 				binDir = "Unavailable"
 			}
 		}
+		logrus.Debugf("Resolved binDir: %s", binDir)
 
 		table := tablewriter.NewWriter(os.Stdout)
 		table.SetHeader([]string{"Variable", "Value"})
