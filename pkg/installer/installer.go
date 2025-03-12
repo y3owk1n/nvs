261b0d698edc4d75488ec053c7630df80a19658d
mtraworld <140996996+mtraworld@users.noreply.github.com>
2025-03-12 14:01:11 +0800

    refactor(installCmd): move installation logic out (#96)

--- pkg/installer/installer.go
+++ pkg/installer/installer.go
@@ -11,8 +11,11 @@ import (
 	"strings"
 	"time"
 
+	"github.com/briandowns/spinner"
 	"github.com/sirupsen/logrus"
 	"github.com/y3owk1n/nvs/pkg/archive"
+	"github.com/y3owk1n/nvs/pkg/releases"
+	"github.com/y3owk1n/nvs/pkg/utils"
 )
 
 var client = &http.Client{Timeout: 15 * time.Second}
@@ -32,6 +35,74 @@ func (pr *progressReader) Read(p []byte) (int, error) {
 		pr.callback(percent)
 	}
 	return n, err
+}
+
+func InstallVersion(alias string, versionsDir string, cacheFilePath string) error {
+	release, err := releases.ResolveVersion(alias, cacheFilePath)
+	if err != nil {
+		return fmt.Errorf("error resolving version: %v", err)
+	}
+
+	logrus.Debugf("Resolved release: %+v", release)
+
+	installName := alias
+	if alias != "stable" && alias != "nightly" {
+		installName = release.TagName
+	}
+
+	logrus.Debugf("Determined install name: %s", installName)
+
+	if utils.IsInstalled(versionsDir, installName) {
+		logrus.Debugf("Version %s is already installed, skipping installation", installName)
+		fmt.Printf("%s %s\n", utils.WarningIcon(), utils.WhiteText(fmt.Sprintf("Version %s is already installed.", utils.CyanText(installName))))
+		os.Exit(0)
+	}
+
+	assetURL, assetPattern, err := releases.GetAssetURL(release)
+	if err != nil {
+		return fmt.Errorf("error getting asset URL: %v", err)
+	}
+
+	logrus.Debugf("Resolved asset URL: %s, asset pattern: %s", assetURL, assetPattern)
+
+	checksumURL, err := releases.GetChecksumURL(release, assetPattern)
+	if err != nil {
+		return fmt.Errorf("error getting checksum URL: %v", err)
+	}
+
+	logrus.Debugf("Resolved checksum URL: %s", checksumURL)
+
+	releaseIdentifier := releases.GetReleaseIdentifier(release, alias)
+	logrus.Debugf("Determined release identifier: %s", releaseIdentifier)
+	fmt.Printf("%s %s\n", utils.InfoIcon(), utils.WhiteText(fmt.Sprintf("Installing Neovim %s...", utils.CyanText(alias))))
+
+	s := spinner.New(spinner.CharSets[14], 100*time.Millisecond)
+	s.Suffix = " 0%"
+	s.Start()
+
+	err = DownloadAndInstall(
+		versionsDir,
+		installName,
+		assetURL,
+		checksumURL,
+		releaseIdentifier,
+		func(progress int) {
+			logrus.Debugf("Download progress: %d%%", progress)
+			s.Suffix = fmt.Sprintf(" %d%%", progress)
+		},
+		func(phase string) {
+			logrus.Debugf("Installation phase: %s", phase)
+			s.Prefix = phase + " "
+			s.Suffix = ""
+		},
+	)
+	s.Stop()
+	if err != nil {
+		return fmt.Errorf("installation failed: %v", err)
+	}
+	logrus.Debug("Installation successful")
+	fmt.Printf("%s %s\n", utils.SuccessIcon(), utils.CyanText("Installation successful!"))
+	return nil
 }
 
 func DownloadAndInstall(versionsDir, installName, assetURL, checksumURL, releaseIdentifier string, progressCallback func(progress int), phaseCallback func(phase string)) error {
