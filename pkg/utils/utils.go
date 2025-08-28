c369d99dc8a4e369a48f8835819bd02dc95fe330
mtraworld <140996996+mtraworld@users.noreply.github.com>
2025-08-28 16:55:24 +0800

    fix: ensure to get the right nvim config directories on windows

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -194,7 +194,7 @@ func GetNvimConfigBaseDir() (string, error) {
 		// fallback to home/.config if LOCALAPPDATA is missing
 	}
 
-	home, err := os.UserHomeDir()
+	home, err := userHomeDir()
 	if err != nil {
 		return "", err
 	}
@@ -338,15 +338,14 @@ func GetInstalledReleaseIdentifier(versionsDir, alias string) (string, error) {
 //	LaunchNvimWithConfig("myconfig")
 //	// Neovim will be launched with configuration located at ~/.config/myconfig
 func LaunchNvimWithConfig(configName string) {
-	home, err := userHomeDir()
+	configDir, err := GetNvimConfigBaseDir()
 	if err != nil {
-		fatalf("Failed to get home directory: %v", err)
+		logrus.Fatalf("Failed to determine config base dir: %v", err)
 	}
-	configDir := filepath.Join(home, ".config", configName)
 
 	info, err := os.Stat(configDir)
 	if os.IsNotExist(err) || !info.IsDir() {
-		fmt.Printf("%s %s\n", ErrorIcon(), WhiteText(fmt.Sprintf("configuration '%s' does not exist in %s", CyanText(configName), CyanText("~/.config"))))
+		fmt.Printf("%s %s\n", ErrorIcon(), WhiteText(fmt.Sprintf("configuration '%s' does not exist in %s", CyanText(configName), CyanText(configDir))))
 		return
 	}
 
