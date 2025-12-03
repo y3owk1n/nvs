0cad8d36e5305b57ea1179cb07bcf4df69d624b8
Kyle Wong <62775956+y3owk1n@users.noreply.github.com>
2025-12-03 22:07:34 +0800

    fix(FindNvimBinary): return binary path on Windows instead of directory

--- pkg/helpers/utils.go
+++ pkg/helpers/utils.go
@@ -263,8 +263,7 @@ func FindNvimBinary(dir string) string {
 			if runtime.GOOS == WindowsOS {
 				if name == "nvim.exe" ||
 					(strings.HasPrefix(name, "nvim-") && filepath.Ext(name) == ".exe") {
-					// Go two levels up: ../../nvim-win64
-					binaryPath = filepath.Dir(filepath.Dir(path))
+					binaryPath = path
 
 					return io.EOF // break early
 				}
